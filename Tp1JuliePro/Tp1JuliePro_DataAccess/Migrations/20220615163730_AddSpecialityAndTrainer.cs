165982e43e2a3b9476ffdadd048d57914ae6fa3e
i <i>
2022-06-15 13:01:03 -0400

    FCT  Trainer et Speciality
    Ajout des propriétés liaisons et navigation

---
+++ Tp1JuliePro/Tp1JuliePro_DataAccess/Migrations/20220615163730_AddSpecialityAndTrainer.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tp1JuliePro_DataAccess.Migrations
+{
+    public partial class AddSpecialityAndTrainer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Speciality",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Speciality", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trainer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Photo = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trainer", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Speciality");
+
+            migrationBuilder.DropTable(
+                name: "Trainer");
+        }
+    }
+}
---
+++ Tp1JuliePro/Tp1JuliePro_Models/Speciality.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp1JuliePro_Models
+{
+    public class Speciality
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Name have to be fill")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Should have between 5 to 15 characters")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+    }
+}
