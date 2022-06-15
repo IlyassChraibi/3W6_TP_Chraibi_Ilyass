bfbab1467b1939f2314fe21bf37290a273c481cf
i <i>
2022-06-15 11:13:59 -0400

    CONF Configuration EntityFramwork
    Installation des packages, ApplicationDbContext, Connexion à la BD.

---
+++ Tp1JuliePro/Tp1JuliePro_DataAccess/Data/JulieProDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp1JuliePro_DataAccess.Data
+{
+    public class JulieProDbContext : DbContext
+    {
+        
+        public JulieProDbContext(DbContextOptions<JulieProDbContext> options) : base(options)
+        {
+        }
+        /*
+        public virtual DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            if (modelBuilder == null)
+                return;
+
+            modelBuilder.GenerateData();
+        }
+        */
+    }
+}
