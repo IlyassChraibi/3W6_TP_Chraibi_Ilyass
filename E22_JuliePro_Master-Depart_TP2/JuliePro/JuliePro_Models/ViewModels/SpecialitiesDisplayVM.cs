7b3d1b880680624c2b059da84392ed397a34add0
Ilyass Chraibi <1925619@cegepmontpetit.ca>
2022-06-24 14:36:34 -0400

    FCT Ajout du controller Speciality auto-génerer avec les views
    Controller et Views

---
+++ E22_JuliePro_Master-Depart_TP2/JuliePro/JuliePro_Models/ViewModels/SpecialitiesDisplayVM.cs
@@ -0,0 +1,29 @@
+﻿using JuliePro_Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuliePro_Models.ViewModels
+{
+    public class SpecialitiesDisplayVM
+    {
+        public SpecialitiesDisplayVM()
+        { }
+        public SpecialitiesDisplayVM(bool isDetails, string pageTitle, string pageHeading, 
+            List<PageLinks> links, Speciality speciality, string submitButtonText = "")
+        {
+            IsDetails = isDetails;
+            GeneralViewInfos = new GeneralViewInfosVM(pageTitle, pageHeading, links, 
+                submitButtonText);
+            Speciality = new SpecialityForDisplayVM(speciality);
+            Id = speciality.Id;
+        }
+
+        public bool IsDetails { get; set; }
+        public int Id { get; set; }
+        public GeneralViewInfosVM GeneralViewInfos { get; set; }
+        public SpecialityForDisplayVM Speciality { get; set; }
+    }
+}
---
+++ E22_JuliePro_Master-Depart_TP2/JuliePro/JuliePro_Models/ViewModels/SpecialitiesUpsertVM.cs
@@ -0,0 +1,34 @@
+﻿using JuliePro_Utility;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuliePro_Models.ViewModels
+{
+    public class SpecialitiesUpsertVM
+    {
+        public SpecialitiesUpsertVM()
+        { }
+        public SpecialitiesUpsertVM(bool isCreate, string pageTitle, string pageHeading, List<PageLinks> links, string submitButtonText, Dictionary<string, SelectList­> selectLists)
+        {
+            IsCreate = isCreate;
+            GeneralViewInfos = new GeneralViewInfosVM(pageTitle, pageHeading, links, submitButtonText);
+            ListForSubject_Id = selectLists["ListForSubject_Id"];
+            ListForPublisher_Id = selectLists["ListForPublisher_Id"];
+        }
+        public SpecialitiesUpsertVM(bool isCreate, string pageTitle, string pageHeading, List<PageLinks> links, string submitButtonText, Dictionary<string, SelectList­> selectLists, Speciality speciality) : this(isCreate, pageTitle, pageHeading, links, submitButtonText, selectLists)
+        {
+            Speciality = speciality;
+            Id = speciality?.Id;
+        }
+        public bool IsCreate { get; set; }
+        public int? Id { get; set; }
+        public GeneralViewInfosVM GeneralViewInfos { get; set; }
+        public Speciality Speciality { get; set; }
+        public IEnumerable<SelectListItem> ListForSubject_Id { get; set; }
+        public IEnumerable<SelectListItem> ListForPublisher_Id { get; set; }
+    }
+}
