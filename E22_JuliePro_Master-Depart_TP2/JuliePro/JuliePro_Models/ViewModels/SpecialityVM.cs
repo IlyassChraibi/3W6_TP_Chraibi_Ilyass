9cfbb398f9cd3e1e970d7ead6703b8099ae70db1
Ilyass Chraibi <1925619@cegepmontpetit.ca>
2022-06-24 11:16:54 -0400

    BUG Object reference not set to an instance of an object.

---
+++ E22_JuliePro_Master-Depart_TP2/JuliePro/JuliePro_Models/ViewModels/SpecialityVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuliePro_Models.ViewModels
+{
+    public class SpecialityVM
+    {
+        public SpecialityVM()
+        {
+
+        }
+        public int Id { get; set; }
+
+        [Display(Name = "SpecialityName")]
+        [Required(ErrorMessage = "RequiredValidation")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "MinMaxLengthValidation")]
+        public string Name { get; set; }
+
+        public List<Trainer> Trainers { get; set; }
+    }
+}
